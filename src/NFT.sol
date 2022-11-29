9836f87014b976f792f3f7381a99976010257862
wighawag <wighawag@gmail.com>
2022-11-29 12:32:42 +0530

    format:fix

--- src/NFT.sol
+++ src/NFT.sol
@@ -1,13 +1,10 @@
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
-
 
 import "solidity-kit/solc_0.8/ERC721/implementations/BasicERC721.sol";
 
-
 contract NFT is BasicERC721 {
-
-  function mint(address to, uint256 tokenID) external {
-    _safeMint(to, tokenID);
-  }
+	function mint(address to, uint256 tokenID) external {
+		_safeMint(to, tokenID);
+	}
 }
